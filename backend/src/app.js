a8875193c7f8ce489e47a4c62268091934f6a225
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2025-09-16 15:40:51 +0200

    dernier livrable

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,12 +15,23 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" }, // <— clé
+  crossOriginEmbedderPolicy: false,                       // évite d’autres blocages
+}));
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+});
+
 
 // Static images
 app.use("/images", express.static(path.join(__dirname, "../images")));
